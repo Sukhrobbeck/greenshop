7474ab92c7af020bafd0a18dc6fb13999c0d2cb4
sukhrobbeckmirzaliyev <mirzaliyevsukhrobbeck@gmail.com>
2023-12-18 21:32:11 +0500

    Husky test

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App: FC = () => {
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
