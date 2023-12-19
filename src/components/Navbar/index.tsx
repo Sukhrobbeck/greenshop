3870f618a547cdf92dd2e7bdc2b9f47b3b81fcf9
sukhrobbeckmirzaliyev <mirzaliyevsukhrobbeck@gmail.com>
2023-12-19 18:50:31 +0500

    First commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,41 @@
+import type { FC } from "react";
+import {
+  SearchOutlined,
+  BellOutlined,
+  ShoppingCartOutlined,
+  LoginOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar: FC = () => {
+  return (
+    <div className="w-[80%] p-[32px] m-auto  flex justify-between border-b border-[#46a358]">
+      <div className="flex items-center">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Ficons%2Flogo.svg?alt=media&token=fc9659d6-f435-43b9-a624-8b0d3a574baa"
+          alt="logo"
+        />
+      </div>
+      <div className="flex gap-6 items-center max-sm:hidden">
+        <h3 className="cursor-pointer">Home</h3>
+        <h3 className="cursor-pointer">Blog</h3>
+      </div>
+      <div className="flex gap-6 items-center max-sm:hidden">
+        <SearchOutlined className="text-[23px] cursor-pointer" />
+        <BellOutlined className="text-[23px] cursor-pointer" />
+        <ShoppingCartOutlined className="text-[23px] cursor-pointer" />
+        <button className="bg-[#46a358] w-[96px] h-[36px] rounded-md text-white flex justify-center items-center gap-2">
+          <LoginOutlined className="text-[23px]" />
+          Login
+        </button>
+      </div>
+      <div className="flex gap-6 items-center hidden max-sm:flex">
+        <SearchOutlined className="text-[23px] cursor-pointer" />
+        <ShoppingCartOutlined className="text-[23px] cursor-pointer" />
+        <MenuOutlined className="text-[23px] cursor-pointer" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import Navbar from "./components/Navbar";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
 );
